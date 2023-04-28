ef89d9605a3b7a7cac537e033cb3e414ac68b7fb
Rafael de Alvarenga Reis <rafaareis.ebac@gmail.com>
2023-04-28 16:44:51 -0300

    Commit 28042023-1644: Parte 5

---
+++ contacts.js
@@ -0,0 +1,56 @@
+const contactsObj = [
+    {
+        id: 1,
+        socialNetwork: 'Facebook',
+        aClass: 'facebook-link',
+        href: 'https://www.facebook.com/',
+        iClass: 'fa-brands fa-facebook',
+        tagI: '<i class="fa-brands fa-facebook"></i>',
+        alt: 'Ícone do Facebook'
+    },
+    {
+        id: 2,
+        socialNetwork: 'Instagram',
+        aClass: 'instagram-link',
+        href: 'https://www.instagram.com/',
+        iClass: 'fa-brands fa-instagram',
+        tagI: '<i class="fa-brands fa-instagram"></i>',
+        alt: 'Ícone do Instagram'
+    },
+    {
+        id: 3,
+        socialNetwork: 'Telegram',
+        aClass: 'telegram-link',
+        href: 'https://web.telegram.org/k/',
+        iClass: 'fa-brands fa-telegram',
+        tagI: '<i class="fa-brands fa-telegram"></i>',
+        alt: 'Ícone do Telegram'
+    },
+    {
+        id: 4,
+        socialNetwork: 'Youtube',
+        aClass: 'youtube-link',
+        href: 'https://www.youtube.com',
+        iClass: 'fa-brands fa-youtube',
+        tagI: '<i class="fa-brands fa-youtube"></i>',
+        alt: 'Ícone do Youtube'
+    },
+    {
+        id: 5,
+        socialNetwork: 'WhatsApp',
+        aClass: 'whatsapp-link',
+        phone:'5512982757380',
+        href:'https://web.whatsapp.com/send?phone=',
+        iClass: 'fa-brands fa-whatsapp',
+        alt: 'Ícone do Whatsapp'
+    },
+    {
+        id: 6,
+        socialNetwork: 'TikTok',
+        aClass: 'tiktok-link',
+        href: 'https://www.tiktok.com',
+        iClass: 'fa-brands fa-tiktok',
+        tagI: '<i class="fa-brands fa-tiktok"></i>',
+        alt: 'Ícone do TikTok'
+    },
+]
