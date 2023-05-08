3e0e9859c7cd856ef0016890b9682dc753ed3a53
Rafael de Alvarenga Reis <rafaareis.ebac@gmail.com>
2023-05-08 17:17:21 -0300

    Commit 08052023-1717: Parte 6

---
+++ carousel.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+    $('.contacts').slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        infinite: true
+    });
+})
--- contacts.js
+++ contacts.js
@@ -4,7 +4,7 @@ const contactsObj = [
         socialNetwork: 'Facebook',
         aClass: 'facebook-link',
         href: 'https://www.facebook.com/',
-        iClass: 'fa-brands fa-facebook',
+        iClass: 'fa-brands fa-facebook-f fa-2xl',
         tagI: '<i class="fa-brands fa-facebook"></i>',
         alt: 'Ícone do Facebook'
     },
@@ -13,7 +13,7 @@ const contactsObj = [
         socialNetwork: 'Instagram',
         aClass: 'instagram-link',
         href: 'https://www.instagram.com/',
-        iClass: 'fa-brands fa-instagram',
+        iClass: 'fa-brands fa-instagram fa-2xl',
         tagI: '<i class="fa-brands fa-instagram"></i>',
         alt: 'Ícone do Instagram'
     },
@@ -22,7 +22,7 @@ const contactsObj = [
         socialNetwork: 'Telegram',
         aClass: 'telegram-link',
         href: 'https://web.telegram.org/k/',
-        iClass: 'fa-brands fa-telegram',
+        iClass: 'fa-brands fa-telegram fa-2xl',
         tagI: '<i class="fa-brands fa-telegram"></i>',
         alt: 'Ícone do Telegram'
     },
@@ -31,7 +31,7 @@ const contactsObj = [
         socialNetwork: 'Youtube',
         aClass: 'youtube-link',
         href: 'https://www.youtube.com',
-        iClass: 'fa-brands fa-youtube',
+        iClass: 'fa-brands fa-youtube fa-2xl',
         tagI: '<i class="fa-brands fa-youtube"></i>',
         alt: 'Ícone do Youtube'
     },
@@ -41,7 +41,7 @@ const contactsObj = [
         aClass: 'whatsapp-link',
         phone:'5512982757380',
         href:'https://web.whatsapp.com/send?phone=',
-        iClass: 'fa-brands fa-whatsapp',
+        iClass: 'fa-brands fa-whatsapp fa-2xl',
         alt: 'Ícone do Whatsapp'
     },
     {
@@ -49,8 +49,38 @@ const contactsObj = [
         socialNetwork: 'TikTok',
         aClass: 'tiktok-link',
         href: 'https://www.tiktok.com',
-        iClass: 'fa-brands fa-tiktok',
+        iClass: 'fa-brands fa-tiktok fa-2xl',
         tagI: '<i class="fa-brands fa-tiktok"></i>',
         alt: 'Ícone do TikTok'
     },
+    {
+        id: 7,
+        socialNetwork: 'Spotify',
+        aClass: 'spotify-link',
+        href: 'https://open.spotify.com/',
+        iClass: "fa-brands fa-spotify fa-2xl",
+        alt: 'Ícone do TikTok'
+    }
 ]
+
+let contacts = contactsObj.map(function(contact) {
+    if(contact.socialNetwork === 'WhatsApp') {
+        return `<li>
+                <div class="contact" >
+                <a class="${contact.aClass}" href="${contact.href}${contact.phone}" id="${contact.socialNetwork}">
+                    <i class="${contact.iClass}" alt="${contact.alt}" id="logo"></i>
+                 </a>
+                </div>
+                </li>`;
+        } else {
+        return `<li>
+                <div class="contact">
+                <a class="${contact.aClass}" href="${contact.href}" id="${contact.socialNetwork}">
+                    <i class="${contact.iClass}" alt=${contact.alt}" id="logo"></i>
+                </a>
+                </div>
+                </li>`;
+    }
+})
+
+document.querySelector('.contacts').innerHTML = contacts.join("");
